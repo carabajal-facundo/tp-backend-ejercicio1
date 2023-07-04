3f28dae19d8db8352f2a6815d27793ffbdaef1ab
carabajal-facundo <facarabajal@hotmail.com>
2023-07-03 23:33:22 -0300

    Se seteo el puerto

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 import express from "express";
-console.log('segunda Prueba');
+
+const app = express();
+// puertos
+    app.set('port', process.env.PORT || 4000);
+    app.listen( app.get('port'), ()=>{console.log('se esta ejecutando en el puerto: '+app.get('port'))} );
+
+// middlwares
+
+// rutas
